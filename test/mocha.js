7bd2881afb55ba28ade8da80b133aac03af84687
linbin <289873007@qq.com>
2019-10-28 11:02:06 +0800

    覆盖率测试

--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai')
-const {sum} = require('../src/index')
+const {sum, min, max} = require('../src/index')
 
 describe('math', () => {
   describe('sum', () => {
@@ -8,6 +8,22 @@ describe('math', () => {
     })
     it('sum 1,2,3,5  equal 11', () => {
       expect(sum(1,2,3,5)).to.equal(11)
+    })
+  })
+  describe('min', () => {
+    it('min 1,2  equal 1', () => {
+      expect(min(1,2)).to.equal(1)
+    })
+    it('min 3,2  equal 2', () => {
+      expect(min(3,2)).to.equal(2)
+    })
+  })
+  describe('max', () => {
+    it('max 1,2  equal 2', () => {
+      expect(max(1,2)).to.equal(2)
+    })
+    it('max 3,2  equal 3', () => {
+      expect(max(3,2)).to.equal(3)
     })
   })
 })
